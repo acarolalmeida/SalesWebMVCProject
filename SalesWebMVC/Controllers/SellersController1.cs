7f1ef8243e0b4e8a5c08df66377dcfa76436533e
Ana Carolina Almeida <ana.carolina.almeida.30@gmail.com>
2023-06-24 19:47:53 -0300

    Sellers Controller

---
+++ SalesWebMVC/Controllers/SellersController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SellersController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
