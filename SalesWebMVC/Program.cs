2dc25ad0e3cec496c4b120d49a901807b820a057
Ana Carolina Almeida <ana.carolina.almeida.30@gmail.com>
2023-06-24 18:14:33 -0300

    Seeding Service

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -1,34 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
-using SalesWebMVC.Data;
+﻿using Microsoft.AspNetCore;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<SalesWebMVCContext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMVCContext"), 
-    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("SalesWebMVCContext")), builder =>
-    builder.MigrationsAssembly("SalesWebMVC")));
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace SalesWebMvc
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
